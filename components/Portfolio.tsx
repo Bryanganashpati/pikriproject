2f8c4efa791b1b66052cf151d612a9ab2be146e0
Bryanganashpati <bryanpnts22@gmail.com>
2026-06-22 18:31:47 +0700

    fix grid

--- components/Portfolio.tsx
+++ components/Portfolio.tsx
@@ -127,7 +127,7 @@ export default function Projects() {
         </div>
 
         {/* Project Cards */}
-        <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-8">
+        <div className="grid md:grid-cols-2 lg:grid-cols-4 gap-8">
           {filteredProjects.map((project) => (
             <div
               key={project.id}
